d94be7f8e175591e363c69e07c8170094fed7e70
SS <forfakework@gmail.com>
2018-12-13 11:35:42 +0530

    basic ui for mobile

---
+++ js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+    App.init();
+});
+var App = {
+    init: function () {
+        this.side.nav(), this.search.bar(), this.navigation(), this.hyperlinks()
+    },
+    title: function (e) {
+        return $(".header>.title").text(e)
+    },
+    side: {
+        nav: function () {
+            this.toggle(), this.navigation()
+        },
+        toggle: function () {
+            $(".ion-ios-navicon").on("touchstart click", function (e) {
+                e.preventDefault(), $(".sidebar").toggleClass("active"), $(".nav").removeClass("active"), $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
+            }), $(".sidebar .sidebar-overlay").on("touchstart click", function (e) {
+                e.preventDefault(), $(".ion-ios-navicon").click(), $(this).removeClass("fadeIn").addClass("fadeOut")
+            })
+        },
+        navigation: function () {
+            $(".nav-left a").on("touchstart click", function (e) {
+                e.preventDefault();
+                var t = $(this).attr("href").replace("#", "");
+                $(".sidebar").toggleClass("active"), $(".html").removeClass("visible"), "home" == t || "" == t || null == t ? $(".html.homepage").addClass("visible") : $(".html." + t).addClass("visible"), App.title($(this).text())
+            })
+        }
+    },
+    search: {
+        bar: function () {
+            $(".header .ion-ios-search").on("touchstart click", function () {
+                var e = ($(".header .search input").hasClass("search-visible"), $(".header .search input").val());
+                return "" != e && null != e ? (App.search.html($(".header .search input").val()), !1) : ($(".nav").removeClass("active"), $(".header .search input").focus(), void $(".header .search input").toggleClass("search-visible"))
+            }), $(".search form").on("submit", function (e) {
+                e.preventDefault(), App.search.html($(".header .search input").val())
+            })
+        },
+        html: function (e) {
+            $(".search input").removeClass("search-visible"), $(".html").removeClass("visible"), $(".html.search").addClass("visible"), App.title("Result"), $(".html.search").html($(".html.search").html()), $(".html.search .key").html(e), $(".header .search input").val("")
+        }
+    },
+    navigation: function () {
+        $(".nav .mask").on("touchstart click", function (e) {
+            e.preventDefault(), $(this).parent().toggleClass("active")
+        })
+    },
+    hyperlinks: function () {
+        $(".nav .nav-item").on("click", function (e) {
+            e.preventDefault();
+            var t = $(this).attr("href").replace("#", "");
+            $(".html").removeClass("visible"), $(".html." + t).addClass("visible"), $(".nav").toggleClass("active"), App.title($(this).text())
+        })
+    }
+};
